87ab4d2210c075660e9864b1ccd4d0ae553c52fc
Osama Hussein <osamahusseindev@gmail.com>
2025-06-16 14:07:50 +0300

    add sign in and sign up pages and style them to enable the user to sign up and sign in, and make the navbar and the footer dont appear whil the user in the sign up and sign in pages

---
+++ src/components/SignInForm.jsx
@@ -0,0 +1,75 @@
+import { Link } from "react-router-dom";
+import "../css/Form.css";
+
+function SignInForm() {
+  function signIn() {
+    const navigate = useNavigate();
+    navigate("/signIn");
+  }
+  return (
+    <div className="sign-container">
+      <div className="form-container">
+        <div className="form main-form">
+          <h2>Welcome back</h2>
+          <p>Sign in to your account</p>
+
+          <div className="form-tabs">
+            <button onClick={signIn} className="active-tab">
+              Sign In
+            </button>
+            <Link to="/signup">Sign Up</Link>
+          </div>
+
+          <form>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="you@example.com"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="•••••••••••"
+                required
+              />
+            </div>
+
+            <div className="form-options">
+              <label className="checkbox-container">
+                <input type="checkbox" />
+                Remember me
+              </label>
+              <a href="#" className="forgot-password">
+                Forgot password?
+              </a>
+            </div>
+
+            <button type="submit" className="btn-primary">
+              Sign In
+            </button>
+          </form>
+
+          <div className="divider">
+            <span>Or continue with</span>
+          </div>
+
+          <button className="google-btn">
+            <img
+              src="https://www.svgrepo.com/show/475656/google-color.svg"
+              alt="Google"
+            />
+            <span>Google</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignInForm;
